6db3d746092d497ba8963a82063dd3a139a7f3ba
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-11-08 15:27:07 +0200

    configure wsgi

--- pp/__init__.py
+++ pp/__init__.py
@@ -1,7 +1,17 @@
 __version__ = '0.1.0'
 from flask import Flask
+from wsgiref.simple_server import make_server
+
 app = Flask(__name__)
+
+@app.route('/')
+def init():
+    return 'It\'s ALIVE!!!'
 
 @app.route('/api/v1/hello-world-31')
 def hello_world():
     return 'Hello World 31'
+
+server = make_server('', 3000, app)
+print('Server is running on http://localhost:3000')
+server.serve_forever()

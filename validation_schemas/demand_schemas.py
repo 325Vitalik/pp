3f84add5f294112295e7298eb410c09235c41b0f
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-12-26 15:31:20 +0200

    Some fixes

--- validation_schemas/demand_schemas.py
+++ validation_schemas/demand_schemas.py
@@ -2,12 +2,8 @@ create_demand_schema = {
 	"$schema": "https://json-schema.org/draft/2019-09/schema",
     "type": "object",
     "additionalProperties": False,
-    "required": ["user_id", "medicine_id", "amount"],
+    "required": ["medicine_id", "amount"],
     "properties": {
-      "user_id": {
-        "type": "string",
-        "pattern": "[a-zA-Z0-9]{8}(-[a-zA-Z0-9]{4}){3}-[a-zA-Z0-9]{12}"
-      },
       "medicine_id": {
         "type": "string",
         "pattern": "[a-zA-Z0-9]{8}(-[a-zA-Z0-9]{4}){3}-[a-zA-Z0-9]{12}"
